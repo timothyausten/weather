c5c311fc362ac2937343d9afa8cbcb6f4a5741d1
Timothy Austen <dfdfdfcvcvcv@gmail.com>
2017-12-05 11:43:37 +0200

    Got a rudimentary for-loop working

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
+/*global $,console*/
+/* https://eslint.org/docs/user-guide/configuring */
+/*eslint no-console: "off", no-unused-vars: "off"*/
+
+
+var APIparams = {};
+
+APIparams.example = {
+    datasetid: 'GSOM',
+    stationid: 'GHCND:USC00010008',
+    units: 'standard',
+    startdate: '2010-05-01',
+    enddate: '2010-05-31'
+};
+APIparams.orchards = {
+    datasetid: 'GSOM',
+    stationid: 'GHCND:US1WACK0003',
+    units: 'standard',
+    startdate: '2010-05-01',
+    enddate: '2010-05-31'
+};
 
 function ajaxResponse(response) {
+    var i, datatype, value, datatypeAndValue;
     var beautifiedJSON = JSON.stringify(response, null, 4);
-    var precipitation = response.responseJSON.results[8].value;
-    $('#output').html(precipitation);
     console.log(response);
+    for (i=0;i<response.responseJSON.results.length;i++) {
+        datatype = response.responseJSON.results[i].datatype;
+        value = response.responseJSON.results[i].value;
+        datatypeAndValue = datatype +  ": " + value;
+        $('#output').append(datatypeAndValue);
+    }
 }
 
 function buildUrl() {
     // GSOM dataset (Global Summary of the Month) for GHCND station USC00010008, for May of 2010 with standard units
     var baseUrl, params, url;
     baseUrl = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?';
-    params = {
-        datasetid: 'GSOM',
-        stationid: 'GHCND:USC00010008',
-        units: 'standard',
-        startdate: '2010-05-01',
-        enddate: '2010-05-31'
-    };
+    params = APIparams.orchards;
     // Produces, for example, datasetid=GSOM&stationid=GHCND:USC00010008&units=standard&startdate=2010-05-01&enddate=2010-05-31
     params = $.param(params);
     url = baseUrl + params;
